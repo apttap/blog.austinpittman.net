44f472f032ccfb925e4698a868ddf4b548bd9bad
Austin Pittman <pittman.austin@gmail.com>
2023-01-06 12:29:48 -0700

    styling changes

--- renderer/PageShell.tsx
+++ renderer/PageShell.tsx
@@ -4,6 +4,7 @@ import logo from "./logo.svg";
 import { PageContext } from "@local-types/page";
 import { PageContextProvider } from "./usePageContext";
 import style from "@style/page/PageShell.module.scss";
+import { urlToFile } from "vite-plugin-ssr/dist/cjs/utils/urlToFile";
 
 export { PageShell };
 
@@ -18,8 +19,27 @@ const PageShell = function ({
     <PageContextProvider pageContext={pageContext}>
       <Layout>
         <div className={style.layout}>
-          <h1>Austin Pittman</h1>
-          <h2>User Interface Engineer</h2>
+          <svg viewBox={"0 0 115 30"}>
+            <defs>
+              <linearGradient id="lgrad" x1="50%" y1="100%" x2="50%" y2="0%">
+                <stop
+                  offset="0%"
+                  style="stop-color:rgb(26,26,26);stop-opacity:1.00"
+                />
+                <stop
+                  offset="100%"
+                  style="stop-color:rgb(193,193,193);stop-opacity:1.00"
+                />
+              </linearGradient>
+            </defs>
+            <text fill="#999" className={style.heading} x={0} y={14}>
+              Austin Pittman
+            </text>
+            <text fill="deeppink" className={style.subheading} x={0} y={23}>
+              User Interface Engineer
+            </text>
+          </svg>
+          <h2></h2>
           <nav className={style.pageNav}>
             <ul>
               <li>
@@ -47,9 +67,7 @@ const Layout = function ({ children }: { children: ComponentChildren }) {
   return (
     <div
       style={{
-        display: "flex",
-        maxWidth: 900,
-        margin: "auto",
+        width: "100%",
       }}
     >
       {children}
@@ -75,5 +93,9 @@ const Sidebar = function ({ children }: { children: ComponentChildren }) {
 };
 
 const Content = function ({ children }: { children: ComponentChildren }) {
-  return <div id="page-content" className={style.content}>{children}</div>;
+  return (
+    <div id="page-content" className={style.content}>
+      {children}
+    </div>
+  );
 };
